fc7a580b4e9bee00e11c862e43e05f49e476e783
shimwoan <shimwoan@gmail.com>
2026-07-08 09:30:17 +0900

    feat: add business domains dropdown to nav

--- partials/header.js
+++ partials/header.js
@@ -2,21 +2,43 @@
   var page = (window.NAV_PAGE || 'index');
 
   var logoHref = page === 'index' ? '/' : 'index.html';
-  var links = page === 'index'
+  var domainsPrefix = page === 'index' ? '' : 'index.html';
+
+  var domainLinks = [
+    { key: 'domains-beauty', href: 'domains-beauty.html', label: '뷰티' },
+    { key: 'domains-agriculture', href: 'domains-agriculture.html', label: '농산물' },
+    { key: 'domains-bio', href: 'domains-bio.html', label: '바이오공정' },
+    { key: 'domains-resource', href: 'domains-resource.html', label: '자원순환' }
+  ];
+  var domainsActive = domainLinks.some(function(d) { return d.key === page; });
+
+  var domainsMenuHtml = domainLinks.map(function(d) {
+    var isActive = d.key === page;
+    return '<li><a href="' + d.href + '"' + (isActive ? ' class="active"' : '') + '>' + d.label + '</a></li>';
+  }).join('\n            ');
+
+  var dropdownLi =
+    '<li class="nav-dropdown">\n' +
+    '        <a href="' + domainsPrefix + '#domains"' + (domainsActive ? ' class="active"' : '') + '>사업 분야</a>\n' +
+    '        <ul class="nav-dropdown-menu">\n' +
+    '            ' + domainsMenuHtml + '\n' +
+    '        </ul>\n' +
+    '      </li>';
+
+  var otherLinks = page === 'index'
     ? [
         { href: 'about.html', label: 'About Us' },
-        { href: '#domains', label: 'Technology' },
         { href: '#', label: 'Product' },
         { href: '#', label: 'Company' }
       ]
     : [
-        { href: 'about.html', label: 'About Us', active: true },
-        { href: 'index.html#domains', label: 'Technology' },
+        { href: 'about.html', label: 'About Us', active: page === 'about' },
         { href: 'index.html', label: 'Product' },
         { href: 'index.html', label: 'Company' }
       ];
 
-  var linksHtml = links.map(function(l) {
+  var aboutLi = '<li><a href="' + otherLinks[0].href + '"' + (otherLinks[0].active ? ' class="active"' : '') + '>' + otherLinks[0].label + '</a></li>';
+  var restLiHtml = otherLinks.slice(1).map(function(l) {
     return '<li><a href="' + l.href + '"' + (l.active ? ' class="active"' : '') + '>' + l.label + '</a></li>';
   }).join('\n      ');
 
@@ -25,7 +47,9 @@
     '  <div id="nav-inner">\n' +
     '    <a href="' + logoHref + '" class="nav-logo">FinSide</a>\n' +
     '    <ul class="nav-links">\n' +
-    '      ' + linksHtml + '\n' +
+    '      ' + aboutLi + '\n' +
+    '      ' + dropdownLi + '\n' +
+    '      ' + restLiHtml + '\n' +
     '    </ul>\n' +
     '  </div>\n' +
     '</nav>'
